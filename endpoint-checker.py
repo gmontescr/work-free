1bb060efab13def529cb5c59cbe6574dd523a9b3
gmontescr <39775626+gmontescr@users.noreply.github.com>
2021-02-23 15:38:05 -0600

    Update endpoint-checker.py

--- endpoint-checker.py
+++ endpoint-checker.py
@@ -8,13 +8,15 @@ import requests
 from oauth2client.service_account import ServiceAccountCredentials
 
 
+#function to return all urls in column1
 def getData():
   scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
   credentials = ServiceAccountCredentials.from_json_keyfile_name("APPLICATION_NAME.json", scope)
   client = gspread.authorize(credentials)
   sheet = client.open("NAME_SHEET").sheet1
-  return sheet.get_all_values()
+  return sheet.col_values(1)
 
+#check if the response code is 200 or if its a redirect go and check redirect as well
 def get_status_code(url):
   try:
     response = requests.get(url)
@@ -24,7 +26,8 @@ def get_status_code(url):
       return response.status_code
   except (requests.exceptions.MissingSchema, requests.ConnectionError) as error:
     return '0'
-  
+
+#send slack webhook to alert that one url is not currently working
 def to_slack(slackText):
   endpoint = "webhook-endpoint"
   slack_data = {'text': ""+slackText+""}
@@ -35,15 +38,16 @@ def to_slack(slackText):
     return 'Post to slack error %d\n' % (response.status_code)
   return response
 
+#run main
 def main():
   data = getData()
-  code = get_status_code(*data[i])
+  code = get_status_code(data[i])
  
-  for i in range(1, len(data)):
-    code = get_status_code(*data[i])
+  for i in range(0, len(data)-1):
+    code = get_status_code(data[i])
     if code not in {200,301,302}:
       slackText += str(*data[i]) + '\n' 
-  print(to_slack(slackText))
+  to_slack(slackText)
 
 if __name__ == "__main__":
   main()
